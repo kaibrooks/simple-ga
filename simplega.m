57dc1c7bbe409157d7655e6c299a5c7ac2edc564
Kai Brooks <kaibrooks@gmail.com>
2019-06-20 10:54:44 -0700

    Updated plot options and mutation display

--- simplega.m
+++ simplega.m
@@ -14,6 +14,10 @@
 % This code is not optimized, in the sense that many lines could be
 % combined into a single function and the code minimized. However, since
 % this is a basic algorithm, many lines are independent for clarity.
+%
+% This algorithm relies on much random chance due to roulette selection and 
+% mutation, so different runs can result in significant performance changes
+%
 
 
 % Init
@@ -21,27 +25,33 @@ clc
 close all
 clear all
 format
-rng('default')
+rng('shuffle')
 
 
 %% Options
 
-popSize = 32;               % number of attempts per generation
-maxGenerations = 2000;      % algorithm will run this many times
+popSize = 20;               % number of attempts per generation
+maxGenerations = 1000;      % algorithm will run this many times (more generations gives more chance to reach maximum and break plateaus)
 chromLength = 256;          % length of each chromosome, represents greatest binary value
 f = @(x) x.^2;              % function to maximize
-
-
-fmax = (bi2de(ones(1,chromLength),'left-msb'))^2; % find theoretical max (since we know it)
-
 
 parentRate = 0.5;           % portion of members designated to breed each generation
 eliteRate = 0.1;            % portion of members who are 'elites', and always breed (as a portion of total population, not parents)
 mutationRate = 0.01;        % portion of members who mutate each generation
+highlightRatio = 0.01;      % for plotting: mutation vs average must be more different than this for highlighting (otherwise the plot looks like a mess), 0 to disable
 
-maxLoops = popSize*maxGenerations*chromLength;             % if any loop runs this many times with no changes, abort program. this prevents stalling
+maxLoops = popSize*maxGenerations*chromLength;  % if any loop runs this many times with no changes, abort program. use for debugging
 
-mutationNum = ceil(popSize*mutationRate); % number of members to mutate
+
+fmax = (bi2de(ones(1,chromLength),'left-msb'))^2; % find theoretical max to compare against (since we know it)
+
+mutationNum = ceil(popSize*mutationRate);   % number of members to mutate
+parentNum = ceil(parentRate * popSize);     % total number of parents
+eliteNum = ceil(eliteRate * popSize);       % number of elites
+if rem(parentNum, 2) ~= 0   % make an odd number of parents even so each has a partner
+    parentNum = parentNum + 1;
+end
+
 
 % temp vars to wipe for release
 
@@ -59,18 +69,13 @@ for generation = 1:maxGenerations
     
     % Compute the function
     fitness = f(decVals);    % compute values using our defined function to get fitness scores
-    fitnessHistory(generation) = mean(fitness);  % store each generations average fitness to see changes over time
+    fitHistAvg(generation) = mean(fitness);  % store each generations average fitness to see changes over time
+    fitHistBest(generation)=max(fitness);
+    fitHistWorst(generation)=min(fitness);
+    
     
     %% Breed New Generation
     % -- Breed: Select Parents
-    % Get whole numbers for elites and parents
-    parentNum = ceil(parentRate * popSize);     % total number of parents
-    eliteNum = ceil(eliteRate * popSize);       % number of elites
-    
-    % If we have a non-even number of parents, add one to make it even
-    if rem(parentNum, 2) ~= 0
-        parentNum = parentNum + 1;
-    end
     
     % Create a binary array to determine if a member will be selected to breed
     isParent = zeros(popSize,1);
@@ -81,7 +86,7 @@ for generation = 1:maxGenerations
     isParent(eliteIndex) = 1;                  % mark it in isParent
     
     disp = [isParent fitness c];
-    fitnessHistory;
+    fitHistAvg;
     
     % -- Select the rest via roulette
     % Normalize fitnesses from 0 to 1
@@ -146,7 +151,7 @@ for generation = 1:maxGenerations
         parenta = c(parentIndex(i),:);
         parentb = c(parentIndex(i+1),:);
         
-        for k = 1:4 %TODO fix number - each parent produces two offspring
+        for k = 1:4 %TODO fix number - each parent produces two offspring (4 in total)
             % select crossover point
             crossover = (randi(chromLength)-1); % pick a random point on the chromosome
             
@@ -167,8 +172,8 @@ for generation = 1:maxGenerations
     % delete the temp entry
     newc(1, :) = [];    % cleanup the temp entry
     
-    % mutate
-    % bit flip mutation
+    %% Mutate
+    % bit flip mutation - find a random bit and invert it
     for i = 1:mutationNum
         randChromIndex = randi(chromLength);
         randPopIndex = randi(popSize);
@@ -177,36 +182,46 @@ for generation = 1:maxGenerations
             newc(randPopIndex,randChromIndex) = 1;
             %fprintf('Mutated member %i at position %i - 0->1\n',randChromIndex,randPopIndex)
         else
-           newc(randPopIndex,randChromIndex) = 0;
-           %fprintf('Mutated member %i at position %i - 1->0\n',randChromIndex,randPopIndex)
+            newc(randPopIndex,randChromIndex) = 0;
+            %fprintf('Mutated member %i at position %i - 1->0\n',randChromIndex,randPopIndex)
         end
         
-        
+        % get decimal values for fitness of the mutated member
+        decValsMut = bi2de(newc(randPopIndex,:),'left-msb');
+        fitHistMut(generation)=f(max(decValsMut));  % store to plot later for comparison
         
     end
     
-        
+    
     % write new chromosomes
     c = newc(randperm(size(newc,1)),:); % randomize the population (to ensure random roulette selection)
     
     
-    fprintf('Generation: %4i \t Fitness: %e \t Optimized: %6.2f%%  \n',generation,fitnessHistory(generation), 100*(fitnessHistory(generation)/fmax))
+    fprintf('Generation: %4i \t Fitness: %e \t Optimized: %6.2f%%  \n',generation,fitHistAvg(generation), 100*(fitHistAvg(generation)/fmax))
 end
 
-plot(fitnessHistory)
+% clean up mutation data for plotting
+for i=1:maxGenerations
+    if abs(fitHistMut(i)-fitHistAvg(i))/fitHistAvg(i) < highlightRatio ;
+        fitHistMutPlot(i) = 0;
+    else
+        fitHistMutPlot(i) = fitHistMut(i); % copy into new array to preserve original data
+    end
+end
+
+% plot fitness history
+hold on
+figure(1)
+
+plot(fitHistMutPlot,'om')
+plot(fitHistWorst,'.r')
+plot(fitHistBest,'.g')
+plot(fitHistAvg,'b','LineWidth',1)
+
+legend('Significant Mutation','Worst Fitness','Best Fitness','Average Fitness','Location','southeast')
+
+title(['Plot axis minimum: ',num2str( ceil((fitHistAvg(1)/fmax)*100) ),'% (of theoretical max)'])
 xlabel('Generation')
 ylabel('Fitness')
-ylim([fmax*0.8 fmax])
+ylim([ceil(fitHistAvg(1)) fmax]) % unusual axes give more visibility to later generations since the early fitnesses are generally chaotic, but settle quickly
 grid on
-% Shuffle
-
-% Results: Plot
-% Results: Display
-
-
-
-
-
-% Functions
-% Func: Binary to Decimal and back
-% end 1:generation loop
